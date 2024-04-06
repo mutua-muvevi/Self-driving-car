e64b6e326d52e7d6fdc3d3c721b4804e3bd51693
mutua-muvevi <samstore637@gmail.com>
2024-04-07 02:12:54 +0300

    added artificial sensors

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
 const canvas = document.getElementById("myCanvas");
-
-
-canvas.width = 300;
+canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
-const road = new Road(canvas.width/2, canvas.width * 0.92)
-
-const car = new Car(road.getLaneCenter(1), 100, 30, 50)
-car.draw(ctx)
-
-animate()
+animate();
 
 function animate() {
-	car.update()
+	console.log("road borders", road.borders);
+	car.update(road.borders);
 
 	canvas.height = window.innerHeight;
 
-	ctx.save()
-	ctx.translate(0,-car.y)
+	ctx.save();
+	ctx.translate(0, -car.y + canvas.height * 0.7);
 
+	road.draw(ctx);
+	car.draw(ctx);
 
-	road.draw(ctx)
-	car.draw(ctx)
-	requestAnimationFrame(animate)
+	ctx.restore();
+	requestAnimationFrame(animate);
 }
